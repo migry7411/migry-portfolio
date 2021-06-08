63ff79fc2d2dcf254034079dfd461ff47203f323
migry7411 <migry7411@daum.net>
2021-06-08 22:16:26 +0900

    war 배포

--- src/main/java/com/projectmigry/migry/portfolio/MigryPortfolioApplication.java
+++ src/main/java/com/projectmigry/migry/portfolio/MigryPortfolioApplication.java
@@ -2,12 +2,18 @@ package com.projectmigry.migry.portfolio;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class MigryPortfolioApplication {
+public class MigryPortfolioApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MigryPortfolioApplication.class, args);
 	}
 
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(MigryPortfolioApplication.class);
+	}
 }
